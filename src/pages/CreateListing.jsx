46c9a28b1940376e507160e8ed39f29a487b3787
Chudy1 <98897666+Chudy1@users.noreply.github.com>
2022-03-23 06:13:56 +0100

    Update CreateListing.jsx

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -15,6 +15,7 @@ import { v4 as uuidv4 } from "uuid";
 import { toast } from "react-toastify";
 
 function CreateListing() {
+  //eslint-disable-next-line
   const [geolocationEnabled, setGeolocationEnabled] = useState(true);
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
@@ -130,13 +131,12 @@ function CreateListing() {
           (snapshot) => {
             const progress =
               (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log("Upload is " + progress + "% done");
             switch (snapshot.state) {
               case "paused":
-                console.log("Upload is paused");
                 break;
               case "running":
-                console.log("Upload is running");
+                break;
+              default:
                 break;
             }
           },
